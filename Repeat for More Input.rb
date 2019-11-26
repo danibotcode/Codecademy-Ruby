f889043d7febf36e4b79aaae2d73964cf3b40aa9
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-25 20:08:06 -0800

    Create Repeat for More Input.rb
    Add print prompts, variables, and gets.chomps for the user’s last name, city, and state/province. Use last_name as the variable for the user’s last name, city for their city, and state for their state or province. (Hint: prompt the user to provide an abbreviation for their state or province, such as “NY” for New York. This will naturally lead us to use .upcase later!)

---
+++ Repeat for More Input.rb
@@ -0,0 +1,15 @@
+print  "What's your first name?"
+
+first_name = gets.chomp
+
+print "What's your last name?"
+
+last_name = gets.chomp
+
+print "What's your city?"
+
+city = gets.chomp
+
+print "What's your state?"
+
+state = gets.chomp
