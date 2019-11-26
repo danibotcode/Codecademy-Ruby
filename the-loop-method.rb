6ab253d421a53e9738d45d2d5ebf33674ae6c169
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-25 22:40:45 -0800

    Create the-loop-method.rb

---
+++ the-loop-method.rb
@@ -0,0 +1,12 @@
+i = 20
+loop do
+  i -= 1
+  print "#{i}"
+  break if i <= 0
+end
+
+=begin
+
+Replace the ___s in the editor with the appropriate keywords. Check the Hint if you need help!
+
+=end
