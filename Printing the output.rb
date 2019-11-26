f0073aae14c5aadb6cf4a1a8776956fde7b2fe75
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-25 20:09:56 -0800

    Create Printing the output.rb
    Printing the Output
    As you might have noticed, Ruby’s not really returning any feedback to us. We want to be able to see our string formatting in action! For this, we’ll need one more new piece of syntax.
    
    If you define a variable monkey that’s equal to the string "Curious George", and then you have a string that says "I took #{monkey} to the zoo", Ruby will do something called string interpolation and replace the #{monkey} bit with the value of monkey—that is, it will print "I took Curious George to the zoo". We can do the same thing here! For example:

---
+++ Printing the output.rb
@@ -0,0 +1,15 @@
+print  "What's your first name?"
+first_name = gets.chomp
+puts "Your name is #{first_name}!"
+
+print "What's your last name?"
+last_name = gets.chomp
+puts "Your name is #{last_name}!"
+
+print "What's your city?"
+city = gets.chomp
+puts "Your name is #{city}!"
+
+print "What's your state?"
+state = gets.chomp
+puts "Your name is #{state}!"
