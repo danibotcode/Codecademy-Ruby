13e9754460ba7832dcf6bc5db6bd7da8aba30402
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-25 21:12:02 -0800

    Create Equal or Not.rb
    In Ruby, we assign values to variables using =, the assignment operator. But if we’ve already used = for assignment, how do we check to see if two things are equal? Well, we use ==, which is a comparator (also called a relational operator). == means “is equal to.” When you type

---
+++ Equal or Not.rb
@@ -0,0 +1,13 @@
+is_true = 2 != 3
+
+is_false = 2 == 3
+
+=begin
+
+We’ve got two variables 
+in the editor: is_true and is_false. 
+Replace the __ with == or != to make 
+is_true evaluate to true and is_false 
+evaluate to false.
+
+=end
