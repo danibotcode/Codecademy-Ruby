212aaab8573911cc057dd91f0eba17ec2a3757dd
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-25 21:23:14 -0800

    Create Practice Makes Perfect.rb

---
+++ Practice Makes Perfect.rb
@@ -0,0 +1,21 @@
+# test_1 = 77 != 77
+test_1 = false
+
+# test_2 = -4 <= -4
+test_2 = true
+
+# test_3 = -44 < -33
+test_3 = true
+
+# test_4 = 100 == 1000
+test_4 = false
+
+=begin
+
+For this round, we’ll show you
+the comparators and you set each
+variable to true or false depending
+on what value you expect the expression
+to return. Remember: no quotes around true and false!
+
+=end
