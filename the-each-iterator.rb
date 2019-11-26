4517786775a73fbf82dd912191201ea713af4e2c
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-25 23:55:28 -0800

    Create the-each-iterator.rb

---
+++ the-each-iterator.rb
@@ -0,0 +1,12 @@
+array = [1,2,3,4,5]
+
+array.each do |x|
+  x += 10
+  print "#{x}"
+end
+
+=begin
+Take a look at the code in the editor. When you’re pretty sure you know what it’ll do, click Run to test your theory.
+
+theory: loops through the array position, adds '10', and prints the result.
+=end
