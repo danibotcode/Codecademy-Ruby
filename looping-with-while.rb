5575346e9f3302b35421147eff63c3de868f76e2
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-26 00:00:39 -0800

    Create looping-with-while.rb

---
+++ looping-with-while.rb
@@ -0,0 +1,12 @@
+i = 1
+while i <= 50 do
+  print i
+  i+=1
+end
+
+=begin
+objective:
+Use a while loop to print out the numbers 1 through 50, inclusive.
+While the example above counts down, you will want to count up.Use 
+print rather than puts, and don’t forget to increment your variable.
+=end
