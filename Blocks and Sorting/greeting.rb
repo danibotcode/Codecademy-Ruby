a6e27357cd9b78ac9b7f15729a43614bc0482eb4
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-26 22:49:25 -0800

    Create greeting.rb

---
+++ Blocks and Sorting/greeting.rb
@@ -0,0 +1,13 @@
+# Define your method below!
+
+def greeting
+  puts "bitcoin $7000"
+end
+
+
+
+
+# Define your method above this line.
+
+greeting # Ignore this for now. We'll explain
+         # it in the next exercise!
