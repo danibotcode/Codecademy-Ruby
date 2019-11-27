796f3b82d7cd20af41239284f4c5cfa34e69964a
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-26 23:56:00 -0800

    Create key of a different color.rb
    #example code

---
+++ Hashes and Symbols/key of a different color.rb
@@ -0,0 +1,17 @@
+menagerie = { :foxes => 2,
+  :giraffe => 1,
+  :weezards => 17,
+  :elves => 1,
+  :canaries => 4,
+  :ham => 1
+}
+
+
+
+
+
+puts "string".object_id
+puts "string".object_id
+
+puts :symbol.object_id
+puts :symbol.object_id
