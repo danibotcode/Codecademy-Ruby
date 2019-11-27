362e3a7e015f64feaec6dc190ede205edbe36bd1
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 14:17:29 -0800

    Create not my type.rb
    Not My Type
    Perfect! Our program is really coming along.
    
    You might have wondered how we’re going to get our movies and ratings from the user—which come in as strings—into the hash where we want our movies to be symbols and our ratings to be integers. Built-in Ruby magic to the rescue!
    
    Ruby’s .to_sym method can convert a string to a symbol, and .to_i will convert a string to an integer.
    
    Instructions
    1.
    Call .to_sym on your title and .to_i on your rating so that your movie titles are stored as symbols in the hash and the associated ratings are stored as integers.

---
+++ Night at the Movies/not my type.rb
@@ -0,0 +1,25 @@
+movies = {
+  star_wars: 5,
+  }
+
+puts "Input please"
+choice = gets.chomp
+
+
+case choice
+  when "add"
+    puts "enter movie title"
+    title = gets.chomp
+    puts "enter movie rating"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "movie added!"
+  when "update"
+    puts "Updated!"
+  when "display"
+    puts "Movies!"
+  when "delete"
+    puts "Deleted!"
+else
+    puts "Error!"
+end
