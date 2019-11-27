d70f053afd01530fa14c63646b92f714443536f9
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 13:33:15 -0800

    Create more methods-more solutions.rb

---
+++ Hashes and Symbols/more methods-more solutions.rb
@@ -0,0 +1,27 @@
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 3,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+# Add your code below!
+
+movie_ratings.each_key { |k| puts k}
+
+
+
+
+
+
+my_hash = { one: 1, two: 2, three: 3 }
+
+my_hash.each_key { |k| print k, " " }
+# ==> one two three
+
+my_hash.each_value { |v| print v, " " }
+# ==> 1 2 3
