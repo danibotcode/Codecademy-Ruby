f585d4fc72ceffb49eb3e344aab6040ea2ec4229
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-26 22:48:34 -0800

    Create method syntax.rb

---
+++ Blocks and Sorting/method syntax.rb
@@ -0,0 +1,16 @@
+def puts_1_to_10
+  (1..10).each { |i| puts i }
+end
+
+puts_1_to_10 # Ignore this for now. We'll explain it soon!
+
+=begin
+Method Syntax
+Methods are defined using the keyword def (short for “define”). Methods have three parts:
+
+The header, which includes the def keyword, the name of the method, and any arguments the method takes. (We’ll get to arguments in the next section)
+The body, which is the code block that describes the procedures the method carries out. The body is indented two spaces by convention (as with for, if, elsif, and else statements)
+The method ends with the end keyword.
+Here’s an example of what the syntax would look like for a simple function, welcome, that just prints “Welcome to Ruby!” to the console:
+
+=end
