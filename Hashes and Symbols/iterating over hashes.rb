1d288c70724380b4353f43cda95e11c96635ed8b
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-26 23:51:56 -0800

    Create iterating over hashes.rb
    my_hash.each do |key, value|
      puts my_hash[]
    end
    
    I had attempted something like this, but it appears I needed to put the key in the square brackets

---
+++ Hashes and Symbols/iterating over hashes.rb
@@ -0,0 +1,11 @@
+matz = { "First name" => "Yukihiro",
+  "Last name" => "Matsumoto",
+  "Age" => 47,
+  "Nationality" => "Japanese",
+  "Nickname" => "Matz"
+}
+
+
+matz.each do |key, value|
+  puts matz[key]
+end
