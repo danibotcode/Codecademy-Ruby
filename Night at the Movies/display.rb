9dcc15e99012037a982f15f10c7eff44e529fb0a
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 14:31:10 -0800

    Create display.rb
    solved, the .to_sym creates symbols with spaces somehow

---
+++ Night at the Movies/display.rb
@@ -0,0 +1,46 @@
+movies = {
+  star_wars: 5,
+  }
+
+puts "Input please"
+choice = gets.chomp
+
+
+case choice
+  when "add"
+    puts "enter movie title"
+    title = gets.chomp
+    puts "enter movie rating"
+    rating = gets.chomp
+    
+    puts "movie added!"
+    if movies[title.to_sym] == nil
+      movies[title.to_sym] = rating.to_i
+    else
+      puts "not added! that movie already exists"
+    end
+  
+  when "update"
+    puts "enter movie title to be updated"
+    title = gets.chomp
+    if movies[title.to_sym] == nil
+      puts "that movie does not exist"
+    else
+      puts "please enter the new rating"
+      rating = gets.chomp
+      movies[title] = rating.to_i
+      puts "movie updated!"
+    end
+      
+  when "display"
+      movies.each  do |movie, rating|
+        puts "#{movie}: #{rating}"
+      end
+  
+  
+    
+  when "delete"
+    puts "Deleted!"
+else
+    puts "Error!"
+end
