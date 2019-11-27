bf671e42eb82cc8f5e076311f93c8386346065cd
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-26 22:42:08 -0800

    Create sorting the hash.rb

---
+++ Data Structures/sorting the hash.rb
@@ -0,0 +1,13 @@
+puts "Enter a phrase you'd like to analyze: "
+text = gets.chomp
+
+words = text.split
+
+frequencies = Hash.new(0)
+
+words.each { |word| frequencies[word] += 1 }
+
+frequencies = frequencies.sort_by do |word, count|
+  count
+end
+frequencies.reverse!
