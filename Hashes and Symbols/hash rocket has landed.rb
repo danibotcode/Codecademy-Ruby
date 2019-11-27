a660c6eafebe409ef96c318da824f91f845c3e16
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 13:20:41 -0800

    Create hash rocket has landed.rb
    updated hashes to new 1.9 syntax

---
+++ Hashes and Symbols/hash rocket has landed.rb
@@ -0,0 +1,13 @@
+=begin
+However, the hash syntax changed in Ruby 1.9. Just when you were getting comfortable!
+
+The good news is that the changed syntax is easier to type than the old hash rocket syntax, 
+and if you’re used to JavaScript objects or Python dictionaries, it will look very familiar:
+=end
+
+movies = {
+  back_to_the_future:  "solid 5/7 perfect score",
+  halloween:  "when will Mike Meyers play Micheal Meyers",
+  jaws:  "we're gonna need a bigger boat",
+  }
+
