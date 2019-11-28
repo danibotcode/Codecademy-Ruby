a80fd45acc5919206bd9b3d0f76d351c7260e595
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 22:22:48 -0800

    Create Creating a Lambda.rb
    Create a lambda called first_half that checks if a hash value is less than (that is, earlier in the alphabet than) “M”. (No need to do anything with the crew hash yet.) Make sure to use a capital “M,” since we’ll be checking capitalized names! Because it will be checking a hash, your lambda should include |key, value| instead of just |value|.

---
+++ Blocks, Procs, and Lambdas/Creating a Lambda.rb
@@ -0,0 +1,15 @@
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+# Add your code below!
+
+first_half = lambda { |key, value| value < "M"}
+
+#Had this correct the whole time, just needed to READ the question more closely. "if a hash value is less than 'M'"
