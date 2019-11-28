e8fc1ae1d9cba8a8d8e1dc6d1522a87b3a5e1a2b
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 22:23:35 -0800

    Create Passing Your Lambda to a Method.rb
    easy.
    
    To finish up, let’s go ahead and create a variable called a_to_m and set it equal to calling .select on crew, and pass in your first_half lambda to filter for the names that are before “M” in the alphabet. Remember to pass &first_half to convert your lambda to a block!
    
    puts a_to_m at the end of the file in order to see the final contents of the array.

---
+++ Blocks, Procs, and Lambdas/Passing Your Lambda to a Method.rb
@@ -0,0 +1,15 @@
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+# Add your code below!
+
+first_half = lambda { |key, value| value < "M"}
+
+a_to_m = crew.select(&first_half)
