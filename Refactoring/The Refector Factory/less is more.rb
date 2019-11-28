5f9b8efdbf118e2cfdc994a06cba80ab50f3d06d
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 17:22:19 -0800

    Create less is more.rb

---
+++ Refactoring/The Refector Factory/less is more.rb
@@ -0,0 +1,11 @@
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+   return "n must be an integer." unless n.is_a? Integer
+   return "n must be greater than 0." unless n > 0
+   Prime.first n
+end
+
+first_n_primes(10)
+
+#line 6 I removed the uncecessary return statement
