150611ac16cc02de824cc03aa8e64b59e461b972
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-11-27 17:14:03 -0800

    Create what you will fix.rb

---
+++ Refactoring/The Refector Factory/what you will fix.rb
@@ -0,0 +1,16 @@
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+
+  unless n.is_a? Integer
+    return "n must be an integer."
+  end
+
+  if n <= 0
+    return "n must be greater than 0."
+  end
+  
+  return Prime.first n
+end
+
+first_n_primes(10)
