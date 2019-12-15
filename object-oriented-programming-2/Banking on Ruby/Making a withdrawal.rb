df3740971f5cebd2e0f7b8efce0bf9cb637f6492
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-12-15 14:01:47 -0800

    Create Making a withdrawal.rb
    Add a public withdraw method to your class that takes two parameters, pin_number and amount. If pin_number matches pin, your method should subtract the amount from the balance and puts "Withdrew #{amount}. New balance: $#{@balance}." Otherwise, it should puts pin_error.

---
+++ object-oriented-programming-2/Banking on Ruby/Making a withdrawal.rb
@@ -0,0 +1,41 @@
+class Account
+  attr_reader :name
+  attr_reader :balance
+  
+  def initialize(name, balance=100)
+  @name = name
+  @balance = balance
+  end
+  
+  public
+  def display_balance(pin_number)
+    @pin_number = pin_number
+    if pin_number == pin()
+      puts "Balance: $#{@balance}."
+    else puts pin_error()
+    end
+  end
+  
+  public
+  def withdraw(pin_number, amount)
+    @amount = amount
+    @pin_number = pin_number
+    if pin_number == pin()
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
+    else puts pin_error()
+    end
+  end
+  
+  
+  
+  private
+  def pin
+    @pin = 1234
+  end
+  
+  private
+  def pin_error
+    return "Access denied: incorrect PIN."
+  end
+  
+end
