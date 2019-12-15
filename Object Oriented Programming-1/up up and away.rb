bfa55964ac9568c2178cf408dcfe259dd5e79500
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-12-15 12:13:14 -0800

    Create up up and away.rb

---
+++ Object Oriented Programming-1/up up and away.rb
@@ -0,0 +1,21 @@
+class Message
+  @@messages_sent = 0 
+  def initialize(from, to)
+    @from = from
+    @to = to
+    @@messages_sent = @@messages_sent + 1
+  end
+  
+end
+  
+my_message = Message.new("Satoshi", "Hal")
+
+class Email < Message
+  
+  def initialize(from, to)
+    super
+  end
+end
+
+
+
