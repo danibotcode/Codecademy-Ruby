8eae36757b652727d3719473e7926dbb37fd8861
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-12-15 13:28:04 -0800

    Create A Matter of Public Knowledge.rb

---
+++ object-oriented-programming-2/A Matter of Public Knowledge.rb
@@ -0,0 +1,14 @@
+class Application
+  attr_accessor :status
+  def initialize; end
+  # Add your method here!
+  
+  public
+  def print_status
+    puts "All systems go!"
+  end
+  
+  
+  
+  
+end
