36ed7057d571516967ce65ed0b76ed988693e3d7
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-12-15 12:11:58 -0800

    Create inheritance.rb
    Create a second class, Email, that inherits from Message. Give it its own initialize method that takes just one parameter, subject, and has one instance variable, @subject, set equal to subject.

---
+++ Object Oriented Programming-1/inheritance.rb
@@ -0,0 +1,18 @@
+class Message
+  @@messages_sent = 0 
+  def initialize(from, to)
+    @from = from
+    @to = to
+    @@messages_sent = @@messages_sent + 1
+  end
+  
+end
+  
+my_message = Message.new("Satoshi", "Hal")
+
+class Email < Message
+  
+  def initialize(subject)
+    @subject = subject
+  end
+end
